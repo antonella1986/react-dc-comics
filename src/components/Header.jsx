6c078a5604416226d80cc05aacc56ee92bf740e4
antonella1986 <antonella.leo.mail@gmail.com>
2025-03-20 17:43:31 +0100

    add style to menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,16 +5,16 @@ export default function Header() {
 
             <img src="/img/dc-logo.png" alt="" srcset="" />
             <ul>
-                <li><a href="http://">Lorem</a></li>
-                <li><a href="http://">Lorem</a></li>
-                <li><a href="http://">Lorem</a></li>
-                <li><a href="http://">Lorem</a></li>
-                <li><a href="http://">Lorem</a></li>
-                <li><a href="http://">Lorem</a></li>
-                <li><a href="http://">Lorem</a></li>
-                <li><a href="http://">Lorem</a></li>
-                <li><a href="http://">Lorem</a></li>
-                <li><a href="http://">Lorem</a></li>
+                <li><a href="http://">CHARACTERS</a></li>
+                <li><a href="http://">COMICS</a></li>
+                <li><a href="http://">MOVIES</a></li>
+                <li><a href="http://">TV</a></li>
+                <li><a href="http://">GAMES</a></li>
+                <li><a href="http://">COLLECTIBLES</a></li>
+                <li><a href="http://">VIDEOS</a></li>
+                <li><a href="http://">FANS</a></li>
+                <li><a href="http://">NEWS</a></li>
+                <li><a href="http://">SHOP</a></li>
             </ul>
 
         </header>
