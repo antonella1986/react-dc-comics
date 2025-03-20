82cb3d33b42752566beb7968fe8413ec31748511
antonella1986 <antonella.leo.mail@gmail.com>
2025-03-20 17:58:43 +0100

    add social media icons and heading to Footer; update styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,6 +10,12 @@ export default function Footer() {
             </section>
             <section className="footer-bottom">
                 <button className="btn">SIGN-UP NOW!</button>
+                <h3>FOLLOW US</h3>
+                <img src="/img/footer-facebook.png" alt="" srcset="" />
+                <img src="/img/footer-twitter.png" alt="" srcset="" />
+                <img src="/img/footer-youtube.png" alt="" srcset="" />
+                <img src="/img/footer-pinterest.png" alt="" srcset="" />
+                <img src="/img/footer-periscope.png" alt="" srcset="" />
             </section>
         </footer>
     )
