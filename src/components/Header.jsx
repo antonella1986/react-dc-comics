f1e3c1e62019a132ecf7c1eb4c0f095ca83ee31c
antonella1986 <antonella.leo.mail@gmail.com>
2025-03-20 17:36:04 +0100

    update Header component structure and styles; add correct CSS for the menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,22 @@
 export default function Header() {
     //tutto quello che c'è nelle parentesi tonde è JSX, nelle graffe è JS
     return (
-        <header className="text-center">
-            <img src="/logo.png" alt="logo" />
+        <header>
+
+            <img src="/img/dc-logo.png" alt="" srcset="" />
             <ul>
-                <li><a href="http://"></a>Home</li>
-                <li><a href="http://"></a>Home</li>
-                <li><a href="http://"></a>Home</li>
-                <li><a href="http://"></a>Home</li>
+                <li><a href="http://">Lorem</a></li>
+                <li><a href="http://">Lorem</a></li>
+                <li><a href="http://">Lorem</a></li>
+                <li><a href="http://">Lorem</a></li>
+                <li><a href="http://">Lorem</a></li>
+                <li><a href="http://">Lorem</a></li>
+                <li><a href="http://">Lorem</a></li>
+                <li><a href="http://">Lorem</a></li>
+                <li><a href="http://">Lorem</a></li>
+                <li><a href="http://">Lorem</a></li>
             </ul>
+
         </header>
     )
-}
+} 
