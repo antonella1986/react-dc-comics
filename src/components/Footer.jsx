0ba3c3561d6b3ba7b19f13d4c3f1fc406e4aa45f
antonella1986 <antonella.leo.mail@gmail.com>
2025-03-20 17:54:57 +0100

    update Footer component structure and styles; enhance CSS for footer sections

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,16 @@
 export default function Footer() {
     //tutto quello che c'è nelle parentesi tonde è JSX, nelle graffe è JS
     return (
-        <footer className="text-center">
-
+        <footer>
+            <section className="footer-up">
+                content here
+            </section>
+            <section className="footer-middle">
+                content here
+            </section>
+            <section className="footer-bottom">
+                <button className="btn">SIGN-UP NOW!</button>
+            </section>
         </footer>
     )
 }
