1d5f5f2b43b1a5b692045faee6a83c2603e00690
antonella1986 <antonella.leo.mail@gmail.com>
2025-03-21 16:41:29 +0100

    edit cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,7 @@
 export default function Card({ image, title }) {
 
     return(
-        <div className="cards">
+        <div className="cardsCard">
             <img src={image} alt={title} className="comic-card-image" />
             <p className="comic-card-title">{title.toUpperCase()}</p>
         </div>
