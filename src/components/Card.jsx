52a850392e3e8f9fcd225bb7ea7bba61d6a79dea
antonella1986 <antonella.leo.mail@gmail.com>
2025-03-21 16:27:29 +0100

    refactor Card component; simplify structure and update CSS styles

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,10 @@
 export default function Card({ image, title }) {
 
     return(
-        <div className="container-cards">
-            <div className="rowCards">
-                <div className="col-2">
-                    <img src={image} alt={title} className="comic-card-image" />
-                    <p className="comic-card-title">{title.toUpperCase()}</p>
-                </div>
-            </div>
+        <div className="cards">
+            <img src={image} alt={title} className="comic-card-image" />
+            <p className="comic-card-title">{title.toUpperCase()}</p>
         </div>
+
     )
 }
