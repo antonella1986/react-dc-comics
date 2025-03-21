96cb32d79ee8d745767b762b4d74d787cdce1573
antonella1986 <antonella.leo.mail@gmail.com>
2025-03-21 15:53:34 +0100

    edit Card component; update class names and title formatting; adjust CSS styles

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,9 +3,9 @@ export default function Card({ image, title }) {
     return(
         <div className="container-cards">
             <div className="rowCards">
-                <div className="card">
+                <div className="cards">
                     <img src={image} alt={title} className="comic-card-image" />
-                    <p className="comic-card-title">{title}</p>
+                    <p className="comic-card-title">{title.toUpperCase()}</p>
                 </div>
             </div>
         </div>
