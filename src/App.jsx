8e64ff704d6be318de8996f4d09b7d6c69101ce5
antonella1986 <antonella.leo.mail@gmail.com>
2025-03-24 11:20:56 +0100

    edit header component to use dynamic links

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Header from './components/Header.jsx'
 import Main from './components/Main.jsx'
 import Footer from './components/Footer.jsx'
 
 function App() {
 
+  const links = [
+    { name: 'CHARACTERS', url: 'http://characters' },
+    { name: 'COMICS', url: 'http://comics' },
+    { name: 'MOVIES', url: 'http://movies' },
+    { name: 'TV', url: 'http://tv' },
+    { name: 'GAMES', url: 'http://games' },
+    { name: 'COLLECTIBLES', url: 'http://collectibles' },
+    { name: 'VIDEOS', url: 'http://videos' },
+    { name: 'FANS', url: 'http://fans' },
+    { name: 'NEWS', url: 'http://news' },
+    { name: 'SHOP', url: 'http://shop' }
+  ];
+
   return (
     <>
-      <Header />
+      <Header links={links} />
       <Main />
       <Footer />
     </>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,15 @@
-export default function Header() {
+export default function Header({ links }) {
     //tutto quello che c'è nelle parentesi tonde è JSX, nelle graffe è JS
     return (
         <header>
 
             <a href="https://www.dc.com/" target="_blank" rel="noopener noreferrer"><img className="logo" src="/img/dc-logo.png" alt="logo DC Comics" /></a>
             <ul className="titles">
-                <li><a href="http://">CHARACTERS</a></li>
-                <li><a href="http://">COMICS</a></li>
-                <li><a href="http://">MOVIES</a></li>
-                <li><a href="http://">TV</a></li>
-                <li><a href="http://">GAMES</a></li>
-                <li><a href="http://">COLLECTIBLES</a></li>
-                <li><a href="http://">VIDEOS</a></li>
-                <li><a href="http://">FANS</a></li>
-                <li><a href="http://">NEWS</a></li>
-                <li><a href="http://">SHOP</a></li>
+                {links.map((link, index) => (
+                    <li key={index}>
+                        <a href={link.url}>{link.name}</a>
+                    </li>
+                ))}
             </ul>
 
         </header>
